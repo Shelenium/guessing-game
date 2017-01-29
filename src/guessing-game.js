5f314555ebeda71d5044bc4294ff6f98b2d9edbb
Shelenium <elena.shokhan@gmail.com>
2017-01-29 12:46:40 +0300

    guessing-game/done:

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min = 0, max = 0, guessnum = 0) {
+    	this.min = min;
+    	this.max = max;
+    	this.guessnum = guessnum;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
+    	return this;
     }
 
     guess() {
+    	this.guessnum = Math.ceil(this.min + (this.max - this.min)/2);
+    	return this.guessnum;
 
     }
 
     lower() {
+    	this.max = this.guessnum;
+    	return this;
 
     }
 
     greater() {
+    	this.min = this.guessnum;
+    	return this;
 
     }
 }
